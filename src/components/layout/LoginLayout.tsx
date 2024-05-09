22113832afe920c39a8943369a667ecee78938d0
Vasiliev <75292914+watanuki469@users.noreply.github.com>
2024-05-09 09:30:25 +0700

    fix ui login

--- src/components/layout/LoginLayout.tsx
+++ src/components/layout/LoginLayout.tsx
@@ -2,16 +2,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { loginApi } from "../../redux/client/api.Login";
-import { useAppDispatch } from "../../redux/hooks";
-import { TextField } from "@mui/material";
+import bg from '../../assets/home-background.jpg'
+
 
 export default function LoginLayout() {
-    const dispatch = useAppDispatch();
     let navigate = useNavigate()
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
-    const [isShowPassword, setIsShowPassword] = useState(false);
 
     const [loadingAPI, setLoadingAPI] = useState(false);
     const handleLogin = async () => {
@@ -39,48 +37,28 @@ export default function LoginLayout() {
         setLoadingAPI(false);
     }
 
-    const handleGoBack = () => {
-        navigate("/");
-    }
-
-    const handlePressEnter = (event: any) => {
-        if (event && event.key === 'Enter') {
-            handleLogin();
-        }
-    }
-
     return (
-        <div className=" min-h-screen cursor-pointer py-16 px-2 ">
-            <div className="bg-white flex justify-center items-center ">
-                <div className="w-full max-w-lg mx-auto">
+        <div className="min-h-screen cursor-pointer py-16 px-2" style={{ backgroundImage: `url(${bg})` }}>
+            <div className="bg-black text-white bg-opacity-70 ml-auto mr-auto flex justify-center items-center rounded w-6/12  ">
+                <div className="w-full max-w-xl shadow-2xl px-10 py-10 ">
                     <h2 className="text-3xl font-bold mb-4 text-center text-blue-500">Log in</h2>
                     <div className="mb-4 ">Email or Username (eve.holt@reqres.in)</div>
-                    <TextField
-                        type="text"
-                        placeholder="Email or Username"
-                        name="email"
-                        fullWidth
+
+                    <input type="text"
                         value={email}
                         onChange={(event) => setEmail(event.target.value)}
-                        // sx={{border:4,borderColor:'blue',borderRadius:2}}
-                        // color="success"
+                        className="w-full h-14 border-0 text-gray-900 ring-1 rounded-md  ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="Email..."
                     />
+
                     <div className="py-4">Password (12345678)</div>
-                    <div className="relative flex-grow input-password mb-4">
-                        <TextField
-                            type="password"
-                            placeholder="password"
-                            name="password"
-                            fullWidth
-                            value={password}
-                            onChange={(event) => setPassword(event.target.value)}
-                            // sx={{border:4,borderColor:'blue',borderRadius:2}}
-                            // color="success"
-                        />
+                    <input type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        placeholder="Password..."
+                        id="password" name="password" className="w-full h-14 px-3 py-2 border-0 text-gray-900 rounded-md  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
 
-                    </div>
-
-                    <div className="flex justify-center">
+                    <div className="flex justify-center mt-6">
                         <button
                             className={`w-full text-center font-bold  py-2 px-4 mb-4 rounded-md ${email && password ? "bg-blue-500 text-white hover:bg-blue-600" : "bg-gray-300 text-gray-500 cursor-not-allowed"}`}
                             disabled={!email || !password}
@@ -89,25 +67,8 @@ export default function LoginLayout() {
                             {loadingAPI ? <i className="fas fa-circle-notch fa-spin mr-2"></i> : null}Login
                         </button>
                     </div>
-
-                    {/* <div
-                        className={`w-full py-2 px-4 mb-4 text-center rounded-md bg-gray-300 text-gray-500 hover:bg-gray-200 font-bold cursor-not-allowed`}
-                    >
-                        <span className="mr-1">
-                            <i className="fas fa-angle-left"></i>
-                        </span>
-                        <span onClick={handleGoBack} className="cursor-pointer">Go Back</span>
-                    </div> */}
                 </div>
             </div>
-
-            {/* <div className="bg-black">
-                <div className="w-full lg:max-w-5xl xl:max-w-5xl mx-auto aligns-center  ">
-                    <div className=" overflow-hidden">
-                        <Footer />
-                    </div>
-                </div>
-            </div > */}
         </div >
     )
 }
