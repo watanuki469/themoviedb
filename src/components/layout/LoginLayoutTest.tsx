80f37a54c16094377f74d348b748add220c5fcc7
Vasiliev <75292914+watanuki469@users.noreply.github.com>
2024-05-15 17:00:37 +0700

    quick fix vercel

--- src/components/layout/LoginLayoutTest.tsx
+++ src/components/layout/LoginLayoutTest.tsx
@@ -30,7 +30,7 @@ const LoginLayoutTest = () => {
             <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="loginEmail">
-                        Email:
+                        Emaill:
                     </label>
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
